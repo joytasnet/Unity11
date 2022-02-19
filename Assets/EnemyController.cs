8957661617c996294150e20a41b97fcbbb2c9ddb
mjpurin <mjpurin@gmail.com>
2022-02-19 14:36:13 +0900

    NavMeshAgent

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform player;
+    public float traceDist = 10f;
+    NavMeshAgent nav;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        StartCoroutine(CheckDist());
+    }
+    IEnumerator CheckDist() {
+        while (true) {
+            yield return new WaitForSeconds(0.2f);
+            float dist = Vector3.Distance(player.position, transform.position);
+            if(dist < traceDist) {
+                nav.SetDestination(player.position);
+                nav.isStopped = false;
+            } else {
+                nav.isStopped = true;
+            }
+        }
+    }
+}
---
+++ Assets/PlayerController1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
+        transform.Translate(0, 0, Input.GetAxis("Vertical") * 5f * Time.deltaTime);
+    }
+}
